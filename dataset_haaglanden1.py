154c8c24699b54450200d5638d3caf6741812b76
Mohammad <mmfd25285@gmail.com>
2025-09-03 06:01:37 +0330

    🚀 quick commit

--- dataset_haaglanden1.py
+++ dataset_haaglanden1.py
@@ -10,7 +10,7 @@ files =[f'{folder}/{f}' for f in files]
 import glob
 from tqdm import tqdm
 X_all, y_all, subj_all, start_all = [], [], [], []
-for i in tqdm(range(22,len(files))):
+for i in tqdm(range(42,len(files))):
     f = files[i]
     d = np.load(f)
     X_all.append(d["X"].astype(np.float32))
@@ -18,7 +18,7 @@ for i in tqdm(range(22,len(files))):
     #subj_all.append(d["subject_id"])
     #start_all.append(d["start_idx"])
 
-    if (i+1) % int(len(files)//6) == 0 or i+1 == len(files):
+    if (i+1) % 21 == 0 or i+1 == len(files):
         print('save file')
         X_all = np.concatenate(X_all, axis=0)
         y_all = np.concatenate(y_all, axis=0)
